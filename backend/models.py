fe55910d203336cfdef6b7a1c0ca4b78985a610f
Frank Wolf <fwolf@posteo.de>
2020-08-07 21:01:32 +0200

    feat: update models to use a 1-to-1-relationship, implement usage of migrations, execute migration, add reduced copy of trivia seed data in trivia_content.psql

--- backend/models.py
+++ backend/models.py
@@ -1,89 +1,101 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
-database_name = "trivia"
+database_name = "triviadb"
 database_path = "postgres://company_data:rightpassword@{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    migrate.init_app(app, db)
     db.create_all()
 
-'''
-Question
-
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
-
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
-
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
-
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
-
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
-
-  @staticmethod
-  def db_close():
-    db.session.close()
-
-  @staticmethod
-  def count():
-    return Question.query.count()
-
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
-
-  def __str__(self):
-    return str(self.format())
 
 '''
 Category
 
 '''
-class Category(db.Model):  
-  __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
 
-  def __init__(self, type):
-    self.type = type
+class Category(db.Model):
+    __tablename__ = 'categories'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+    category_questions = db.relationship("Question", backref="category")
+
+    def __init__(self, type):
+        self.type = type
+
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
+
+
+'''
+Question
+
+'''
+
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    difficulty = Column(Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def db_close():
+        db.session.close()
+
+    @staticmethod
+    def count():
+        return Question.query.count()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category.type,
+            'difficulty': self.difficulty
+        }
+
+    def __str__(self):
+        return str(self.format())
